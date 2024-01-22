a0f1b9e76a6ec476667eb0895dc31f2ff9e8f78a
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2024-01-22 11:40:28 -0500

    Remove debuging prints

--- server/dist/controllers/authController.js
+++ server/dist/controllers/authController.js
@@ -2,7 +2,6 @@ import asyncHandler from "express-async-handler";
 import axios from "axios";
 import { generateRandomString } from "../utils/cryptoUtils.js";
 const stateKey = "spotify_auth_state";
-//TYPES
 const spotifyLoginUser = (req, res) => {
     //Protection against CSRF
     const state = generateRandomString(16);
@@ -16,57 +15,45 @@ const spotifyLoginUser = (req, res) => {
         state: state,
         show_dialog: "true"
     });
-    console.log(spotifyRedirectParams);
     res.redirect("https://accounts.spotify.com/authorize?" +
         spotifyRedirectParams.toString());
 };
 const getInitialTokens = asyncHandler(async (req, res) => {
-    console.log("inside of getInitialTokens");
-    console.log(req.query);
     const authCode = req.query.code || null;
     const authState = req.query.state || null;
     const storedState = req.cookies ? req.cookies[stateKey] : null;
     if (authState === null || authState !== storedState) {
-        //res.redirect('/#'+errorRedirectParams.toString());
+        res.status(500);
         res.json({ error: 'state_mismatch' });
     }
     else {
-        //dont need it anymore, throw away for security
         res.clearCookie[stateKey];
         const authHeaderStr = 'Basic ' + (Buffer.from(process.env.SPOTIFY_CLIENT_ID +
             ":" + process.env.SPOTIFY_CLIENT_SECRET, "utf-8").toString('base64'));
-        console.log(authHeaderStr);
         const authData = {
             code: authCode,
             redirect_uri: "http://localhost:3000/top",
             grant_type: 'authorization_code'
         };
-        //const tokenurl = "https://accounts.spotify.com/api/token"
-        console.log(authData);
         const { data, status, statusText } = await axios.post("https://accounts.spotify.com/api/token", authData, { headers: { "Authorization": authHeaderStr, "Content-Type": "application/x-www-form-urlencoded" } });
         if (statusText === 'OK' && status === 200) {
-            console.log(data);
             const access_token = data.access_token;
             const refresh_token = data.refresh_token;
             res.json({ access_token, refresh_token }).status(200);
         }
         else {
-            console.log("There was an error getting the mcTokens");
-            console.log(data);
-            console.log(status);
-            console.log(statusText);
-            res.json({ error: 'no tokens :(' }).status(500);
+            console.log("There was an error getting the initial mcTokens");
+            res.status(500);
+            res.json({ error: 'no tokens :(' });
         }
     }
 });
 const refreshToken = asyncHandler(async (req, res) => {
-    console.log("in server refresh tokens");
     const old_refresh_token = req.query.refresh_token;
     const authData = {
         grant_type: 'refresh_token',
         refresh_token: old_refresh_token
     };
-    console.log(old_refresh_token);
     const authHeaderString = 'Basic ' + (Buffer.from(process.env.SPOTIFY_CLIENT_ID +
         ':' + process.env.SPOTIFY_CLIENT_SECRET, "utf-8").toString('base64'));
     try {
@@ -76,8 +63,6 @@ const refreshToken = asyncHandler(async (req, res) => {
             res.json({ access_token }).status(200);
         }
         else {
-            console.log("There was an error refreshing ze tokens");
-            console.log(data);
             res.json({ error: "no new tokens :(" }).status(500);
         }
     }
--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -9,9 +9,6 @@ import { generateRandomString } from "../utils/cryptoUtils.js";
 
 const stateKey:string = "spotify_auth_state";
 
-//TYPES
-
-
 const spotifyLoginUser =  (req:Request,res:Response)
   :void=>{
 
@@ -31,31 +28,24 @@ const spotifyLoginUser =  (req:Request,res:Response)
     show_dialog: "true"
   })
 
-  console.log(spotifyRedirectParams);
-  
   res.redirect("https://accounts.spotify.com/authorize?"+
     spotifyRedirectParams.toString()
   );
 };
 
 const getInitialTokens = asyncHandler( async (req:Request,res:Response): Promise<void>=>{
-  console.log("inside of getInitialTokens");
-  console.log(req.query);
   const authCode = req.query.code || null;
   const authState = req.query.state || null;
   const storedState:string|null = req.cookies ? req.cookies[stateKey] : null;
 
   if(authState === null || authState !== storedState){
-    //res.redirect('/#'+errorRedirectParams.toString());
+    res.status(500);
     res.json({error: 'state_mismatch'});
   }
   else{
-    //dont need it anymore, throw away for security
     res.clearCookie[stateKey];
     const authHeaderStr = 'Basic '+ (Buffer.from(process.env.SPOTIFY_CLIENT_ID+
           ":"+process.env.SPOTIFY_CLIENT_SECRET,"utf-8").toString('base64'))
-
-    console.log(authHeaderStr);
 
     const authData = {
       code: authCode,
@@ -63,8 +53,6 @@ const getInitialTokens = asyncHandler( async (req:Request,res:Response): Promise
       grant_type: 'authorization_code'
     }
 
-    //const tokenurl = "https://accounts.spotify.com/api/token"
-    console.log(authData);
     const {data, status, statusText} = await axios.post(
       "https://accounts.spotify.com/api/token",
       authData,
@@ -73,61 +61,47 @@ const getInitialTokens = asyncHandler( async (req:Request,res:Response): Promise
 
     if(statusText === 'OK' && status === 200){
       
-      console.log(data)
       const access_token = data.access_token;
       const refresh_token = data.refresh_token;
-
 
       res.json({access_token, refresh_token}).status(200);
     }
     else{
-      console.log("There was an error getting the mcTokens");
-      console.log(data);
-      console.log(status);
-      console.log(statusText);
-
-      res.json({error: 'no tokens :('}).status(500);
+      console.log("There was an error getting the initial mcTokens");
+      res.status(500);
+      res.json({error: 'no tokens :('});
     }
   }
 });
 
 const refreshToken = asyncHandler(async (req:Request, res:Response)=>{
-  console.log("in server refresh tokens");
   const old_refresh_token = req.query.refresh_token
   const authData = {
     grant_type: 'refresh_token',
     refresh_token: old_refresh_token
   }
 
-  console.log(old_refresh_token);
-
   const authHeaderString = 'Basic '+ (Buffer.from(process.env.SPOTIFY_CLIENT_ID+
     ':'+process.env.SPOTIFY_CLIENT_SECRET, "utf-8").toString('base64'));
 
   try{
-
   const {data,status,statusText} = await axios.post(
     "https://accounts.spotify.com/api/token",
     authData,
     {headers: {"Authorization": authHeaderString, "Content-Type": "application/x-www-form-urlencoded"}}
   );
-
   
   if(statusText==='OK' && status === 200){
     const access_token = data.access_token;
     res.json({access_token }).status(200);
   }
   else{
-    console.log("There was an error refreshing ze tokens");
-    console.log(data);
     res.json({error: "no new tokens :("}).status(500);
   }
 
 }catch(error){
     console.log(error)
   }
-
-
 })
 
 export {
