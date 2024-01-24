1b28fb59ba2615d47ad7d394d592282c64e2b946
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2024-01-24 10:52:59 -0500

    Make LogOutModal its own comp.

--- frontend/gb-ui/src/components/NavBar.tsx
+++ frontend/gb-ui/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Outlet, useLoaderData,  NavLink, useNavigate} from "react-router-dom";
 import { requestMySpotifyAccount } from "../api";
+import LogOutModal from "./LogOutModal";
 
 export async function loader(){
   const access_token:string|null = localStorage.getItem("access_token");
@@ -40,13 +41,6 @@ export default function NavBar(){
   },[loaderData,navigate])
 
 
-  function handleLogOut(){
-    setShowLogOutModal(false);
-    localStorage.clear();
-    navigate("/");
-    window.location.reload();
-  }
-
   return (
     <div className="max-h-screen h-screen flex flex-col items-center justify-center">
       <Outlet/>
@@ -80,30 +74,7 @@ export default function NavBar(){
         }
         
       </div>
-      {
-        showLogOutModal ?
-          <div className="z-10 fixed h-full w-full left-0 top-0 pt-48 bg-[rgba(0,0,0,.4)]"
-            >
-              <div className="bg-gray-50 m-auto w-10/12 flex justify-center">
-                <div className="flex flex-col text-xl">
-                  <div className="font-bold">
-                    u wanna log out?
-                  </div>
-                  <div>
-                    when you log back in, on the Permissions Page, Click 'not you?' link to switch accounts.
-                    </div>
-                  <div className="flex justify-center">
-                    
-                    <button type="submit" onClick={()=>{handleLogOut()}}className="bg-red-950 text-white p-4 m-4 rounded-md">yeh</button>
-                    <button onClick={()=>{setShowLogOutModal(false)}} className="bg-gray-700 text-white p-4 m-4 rounded-md">nah</button>
-                  </div>
-                </div>
-              </div>
-
-          </div>
-        :
-        null
-      }
+      { showLogOutModal ? <LogOutModal setShowModal={setShowLogOutModal}/> : null }
     </div>
   )
 }
