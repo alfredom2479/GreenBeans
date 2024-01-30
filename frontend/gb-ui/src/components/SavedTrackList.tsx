c6e6df26ad798ebdad01d88f839c78db423f5c74
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2024-01-30 11:17:04 -0500

    Move modal logic into SavedPage

--- frontend/gb-ui/src/components/SavedTrackList.tsx
+++ frontend/gb-ui/src/components/SavedTrackList.tsx
@@ -5,7 +5,8 @@ import { requestSavedTracks } from "../api";
 
 import type {Params} from "react-router-dom";
 
-import { ITrack } from "../interfaces";
+import { ITrack, useHandleListenOnClick } from "../interfaces";
+import { isTrack } from "../utils";
 
 interface URLParams{
   params:Params
@@ -28,18 +29,15 @@ export async function loader({params}:URLParams){
       pageNumber = 0
     }
   }
-  console.log(pageNumber);
   try{
     const data = await requestSavedTracks(pageNumber,50,accessToken);
     console.log(data)
     
     if(data && data.items && Array.isArray(data.items)){
-      console.log("made it to if");
       if(data.items.length === 0){
         if(pageNumber !== 0 ){
           return redirect("/saved/0")
         }
-        return redirect("/link-search");
       }
       return data.items;
     }
@@ -47,6 +45,7 @@ export async function loader({params}:URLParams){
       return [];
     }
   }catch(err){
+    //throw an error that will be caught by errorElement
     console.log("Thre has been an error while loading the saved tracks data");
     console.log(err);
   }
@@ -54,10 +53,9 @@ export async function loader({params}:URLParams){
 
 export default function SavedTrackList(){
 
-  const [showModal, setShowModal] = useState(false);
-  const [modalSongPreviewUrl, setModalSongPreviewUrl] = useState<string>("");
-
   const [savedTracksList, setSavedTracksList] = useState<ITrack[]>([])
+
+  const {handleListenOnClick} = useHandleListenOnClick();
 
   const loaderData = useLoaderData();
 
@@ -84,61 +82,30 @@ export default function SavedTrackList(){
     if(Array.isArray(loaderData)){
 
       const tempTrackList:ITrack[] = [];
+      let possibleTrack:ITrack|null = null
       
       for(let i=0; i < loaderData.length;i++){
-        //console.log(loaderData[i]);
-        const tempTrackInfo:ITrack = {id:"", name:"",artist:"",image:""};
-
-        if(!loaderData[i].track){
-          continue;
+        possibleTrack = isTrack(loaderData[i].track)
+        if(possibleTrack != null){
+          tempTrackList.push(possibleTrack)
         }
-        if(loaderData[i].track.id){
-          tempTrackInfo.id = loaderData[i].track.id;
-        }
-        if(loaderData[i].track.name){
-          tempTrackInfo.name = loaderData[i].track.name
-        }
-        if(loaderData[i].track.artists){
-          if(Array.isArray(loaderData[i].track.artists) && loaderData[i].track.artists.length > 0){
-            tempTrackInfo.artist = loaderData[i].track.artists[0].name;
-          }
-        }
-        if(loaderData[i].track.album && loaderData[i].track.album.images){
-          if(Array.isArray(loaderData[i].track.album.images) && 
-            loaderData[i].track.album.images.length > 0){
-              tempTrackInfo.image = loaderData[i].track.album.images[loaderData[i].track.album.images.length-1].url;
-            }
-        }
-        if(loaderData[i].track.preview_url){
-          tempTrackInfo.url = loaderData[i].track.preview_url;
-        }
-        else if(loaderData[i].track.external_urls && loaderData[i].track.external_urls.spotify){
-          tempTrackInfo.url = loaderData[i].track.external_urls.spotify;
-        }
-        tempTrackList.push(tempTrackInfo);
       }
       setSavedTracksList(tempTrackList);
     }
   },[loaderData]);
 
-  function handleListenOnClick(songPreviewUrl:string|undefined){
-    if(songPreviewUrl === undefined){
-      console.log("Song preview url is undefined");
-      return;
-    }
-    setModalSongPreviewUrl(songPreviewUrl);
-    setShowModal(true);
-    return;
-  }
-
   return(
   <>
   <div className="flex">
-        <NavLink 
+        {pageNumber !== 0 ?
+          <NavLink 
           to={`/saved/${prevPageNumber}`}
           className="flex-1 items-center justify-center bg-stone-900 hover:text-purple-600 text-purple-200 text-xl font-bold p-1  text-center border-white border-2 border-l-0 hover:border-purple-600"
           >prev
         </NavLink>
+        : 
+          null
+        }
         <NavLink 
           to={`/saved/${nextPageNumber}`}
           className="flex-1 items-center justify-center bg-stone-900 hover:text-purple-600 text-purple-200 text-xl font-bold p-1  text-center border-white border-2 border-r-0 border-l-2 hover:border-purple-600"
@@ -164,17 +131,6 @@ export default function SavedTrackList(){
         })}
       </ul>
     </div>
-{showModal ?
-      <div className="z-10 fixed h-full w-full left-0 top-0 pt-48 bg-[rgba(0,0,0,.4)]"
-      onClick={()=>{setShowModal(false)}}>
-        <div className="bg-gray-50 m-auto w-10/12 flex justify-center">
-          <iframe className="m-2" src={modalSongPreviewUrl}/>
-        </div>
-    
-      </div>
-      :
-      null
-    }
     </>
   )
 
--- frontend/gb-ui/src/pages/SavedPage.tsx
+++ frontend/gb-ui/src/pages/SavedPage.tsx
@@ -1,10 +1,25 @@
-//import {useState} from 'react';
+import {useState} from 'react';
 import { Outlet } from "react-router-dom";
 
+import SongPreviewModal from "../components/SongPreviewModal";
+import { ListenOnClickContextType } from "../interfaces";
 
 export default  function SavedPage(){
 
-  
+  const [showModal, setShowModal] = useState(false);
+  const [modalSongPreviewUrl, setModalSongPreviewUrl] = useState<string>("");
+
+
+
+  function handleListenOnClick(songPreviewUrl:string|undefined){
+    if(songPreviewUrl === undefined){
+      console.log("Song preview url is undefined");
+      return;
+    }
+    setModalSongPreviewUrl(songPreviewUrl);
+    setShowModal(true);
+    return;
+  }
   return(
     <div className="h-full pb-16 flex flex-col">
       <div className="basis-24 p-4 flex flex-col justify-center items-center">
@@ -14,7 +29,15 @@ export default  function SavedPage(){
         <div className="flex">
         </div>
       </div>
-      <Outlet/>
+      <Outlet context={{handleListenOnClick} satisfies ListenOnClickContextType}/>
+      {showModal ?
+      <SongPreviewModal
+        setShowModal={setShowModal}
+        songPreviewUrl= {modalSongPreviewUrl}
+      />
+      :
+      null
+    }
     </div>
   )
 }
