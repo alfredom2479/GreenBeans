6138d18792715c359ca230a697e18ea5a9a09f64
alfredo <alfredomendez2479@gmail.com>
2024-12-22 06:25:05 -0500

    fix mobile navlink 'search' bug & remove search results border

--- frontend/gb-ui/src/components/navigation/SideBar.tsx
+++ frontend/gb-ui/src/components/navigation/SideBar.tsx
@@ -24,7 +24,8 @@ export default function SideBar({setShowSidebar, currUser, setShowLogOutModal}:p
                   </div> 
                 </div>
               : null }
-            <SideBarNavItem path="/" name="Use Share Link" isDisabled={false}/>
+            <SideBarNavItem path="/" name="Search" isDisabled={false}/>
+            <SideBarNavItem path="/link" name="Use Share Link" isDisabled={false}/>
             <SideBarNavItem path="/saved" name="My Saved Tracks" isDisabled={currUser === null}/>
             <SideBarNavItem path="/top" name="My Top Tracks" isDisabled={currUser === null}/>
             {currUser == null ?
